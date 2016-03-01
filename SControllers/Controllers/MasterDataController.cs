cdf1cf44f8dcdcaf02f49a9cace40e1d0cfc3663
HARSHAD557 <HARSHAD_DHAYGUDE001@YAHOO.COM>
2016-03-01 14:21:35 +0530

    Angular

---
+++ SControllers/Controllers/MasterDataController.cs
@@ -0,0 +1,123 @@
+﻿namespace SimpleAngularJSAppWithMVC.Controllers
+{
+    using System.Web.Mvc;
+
+    using AngularMVC.DbUtil;
+
+    using Newtonsoft.Json;
+
+    public class MasterDataController : Controller
+    {
+
+        public MasterDataController()
+        {
+
+        }
+
+        [System.Web.Http.HttpGet]
+        public ActionResult getCountryList()
+        {
+            return Json(new DbUtility().GetAllDocuments("Master_Country").Replace("ObjectId(", "").Replace(")", ""), JsonRequestBehavior.AllowGet);
+        }
+
+        [System.Web.Http.HttpPost]
+        public ActionResult getStateList(string countryName)
+        {
+            if (countryName != null)
+            {
+                string temp = (string)JsonConvert.DeserializeObject(countryName);
+                return Json(
+                    new DbUtility().GetDocumentById("Master_State", "CountryName", temp),
+                    JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
+
+        [System.Web.Http.HttpPost]
+        public ActionResult getDistrictList(string stateName)
+        {
+            if (stateName != null)
+            {
+                string temp = (string)JsonConvert.DeserializeObject(stateName);
+                return Json(
+                    new DbUtility().GetDocumentById("Master_District", "StateName", temp),
+                    JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
+
+        [System.Web.Http.HttpPost]
+        public ActionResult getTalukaList(string districtName)
+        {
+            if (districtName != null)
+            {
+                string temp = (string)JsonConvert.DeserializeObject(districtName);
+                return Json(
+                    new DbUtility().GetDocumentById("Master_Taluka", "DistrictName", temp),
+                    JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
+
+        //[System.Web.Http.HttpGet]
+        //public ActionResult getCategoryList()
+        //{
+        //    return Json(((MasterCategoryService) iservCategoryService).GetAll(), JsonRequestBehavior.AllowGet);
+        //}
+
+        //[System.Web.Http.HttpPost]
+        //public ActionResult getCasteList(string categoryName)
+        //{
+        //    if (categoryName != null)
+        //    {
+        //        string tempcategoryName = (string)JsonConvert.DeserializeObject(categoryName);
+        //        return Json(
+        //            ((MasterCasteService)this.iservCasteService).getCasteList("CategoryName", tempcategoryName),
+        //            JsonRequestBehavior.AllowGet);
+        //    }
+        //    return null;
+        //}
+
+        //[System.Web.Http.HttpGet]
+        //public ActionResult getReligionList()
+        //{
+        //    return Json(((MasterReligionService)this.iservReligionService).GetAll(), JsonRequestBehavior.AllowGet);
+        //}
+
+        //[System.Web.Http.HttpGet]
+        //public ActionResult getSocialReservationList()
+        //{
+        //    return Json(((MasterSocialReservationService)this.iservSocialReservationService).GetAll(), JsonRequestBehavior.AllowGet);
+        //}
+
+        //[System.Web.Http.HttpGet]
+        //public ActionResult getMaritalStatusList()
+        //{
+        //    return Json(((MasterMaritalStatusService)this.iservMasterMaritalStatusService).GetAll(), JsonRequestBehavior.AllowGet);
+        //}
+
+        //[System.Web.Http.HttpGet]
+        //public ActionResult getMarksheetList()
+        //{
+        //    return Json(((MasterMarksheetService)this.iservMasterMarksheetService).GetAll(), JsonRequestBehavior.AllowGet);
+        //}
+
+        //[System.Web.Http.HttpGet]
+        //public ActionResult getGenderList()
+        //{
+        //    return Json(((MasterGenderService)this.iservMasterGenderService).GetAll(), JsonRequestBehavior.AllowGet);
+        //}
+
+        //[System.Web.Http.HttpGet]
+        //public ActionResult getAreaList()
+        //{
+        //    return Json(((MasterAreaService)this.iservMasterAreaService).GetAll(), JsonRequestBehavior.AllowGet);
+        //}
+
+        //[System.Web.Http.HttpGet]
+        //public ActionResult getPHTypeList()
+        //{
+        //    return Json(((MasterPHTypeService)this.iservMasterPHTypeService).GetAll(), JsonRequestBehavior.AllowGet);
+        //}
+    }
+}
---
+++ SControllers/Controllers/UserInfoController.cs
@@ -0,0 +1,51 @@
+﻿using System.Web.Mvc;
+
+namespace SimpleAngularJSAppWithMVC.Controllers
+{
+    using AngularMVC.DbUtil;
+
+    using Newtonsoft.Json;
+
+    public class UserInfoController : Controller
+    {
+        public string SaveUserInfoDetails(dynamic UserInfoDetails)
+        {
+            
+            new DbUtility().SaveDocument(UserInfoDetails, "UserInfo");
+            return JsonConvert.SerializeObject(new {Result=1,Message="Success",Status=1});
+        }
+        public JsonResult getUserInfoDetails()
+        {
+            return Json(new DbUtility().GetAllDocuments("UserInfo").Replace("ObjectId(","").Replace(")",""),JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult getCheckBoxCheckedFromDB()
+        {
+            string[] checkboxchecked = {"A","B","C"};
+            return Json(JsonConvert.SerializeObject(checkboxchecked), JsonRequestBehavior.AllowGet);
+        }
+        [System.Web.Http.HttpPost]
+        public JsonResult validateLoginDetails(dynamic loginDetails)
+        {
+            dynamic ld = JsonConvert.DeserializeObject(loginDetails);
+            string[] keys = { "email", "Password" };
+            string[] values = { ld.email, ld.password };
+            if (loginDetails != null)
+            {
+                return Json(new DbUtility().findbyMultipleKey(keys, values, "UserInfo"), JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
+        [System.Web.Http.HttpPost]
+        public JsonResult getProductList()
+        {
+            return Json(new DbUtility().GetAllDocumentsWithObjectId("Master_Products"), JsonRequestBehavior.AllowGet);
+
+        }
+        [System.Web.Http.HttpPost]
+        public string placeOrder(string Id,string productList)
+        {
+            new DbUtility().updateOrdersofUser(Id, productList, "UserInfo");
+            return "Success";
+        }
+    }
+}
